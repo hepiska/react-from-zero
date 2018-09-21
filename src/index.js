19f5344c647489bf50f867035ec75fff9ed5157b
hepiska franatagola <ego1403@gmail.com>
2018-09-22 01:52:26 +0700

    webpack confiq

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-const a = 1
-console.log(a);
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>write in react!!!!!!!</div>
+        )
+    }
+}
+
+
+ReactDOM.render(<App/>, document.getElementById('index'))
